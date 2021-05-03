3e5b1bbf7f8dbc3a60fbe27fe96d03502dbd606d
Theodore Stansbury <tstansbu.27@gmail.com>
2021-05-03 16:49:46 -0400

    Updated input validation and fetch link

--- index.js
+++ index.js
@@ -1,47 +1,59 @@
 const getDogImages = (breed) => {
-    fetch(`https://dog.ceo/api/breed/${breed}/images/random/5`)
+    let subBreed = breed.split(/(\s+)/);
+    let breedOf = subBreed[0];
+    let sub = subBreed[2];
+
+    if (sub === undefined){
+        link = `https://dog.ceo/api/breed/${breedOf}/images/random/5`
+    }
+    else {
+        link = `https://dog.ceo/api/breed/${breedOf}/${sub}/images/random/5`
+    }
+
+    fetch(link)
     .then(response=> response.json())
     .then(responseJson => displayDogs(responseJson) )
     .catch(error => console.log('Sorry! An error has occurred.'))
 }
 
-
 const displayDogs = (responseJson) => {
-    let breed = $('#breed').val();
+    let breed = $('#breed').val().charAt(0).toUpperCase() + $('#breed').val().slice(1);
 
     for (let i=0; i<responseJson.message.length; i++){
         if (responseJson.status === "error") {
             $('.dog-image').empty() && $('.error-container').empty();
-            $('.error-container').prepend('Not a breed of a dog. Please try again.');
+            $('.error-container').prepend(`${breed} is not a breed of a dog. Please try again.`);
         }
         else {
             $('.dog-image').append(
-                `<img src=${responseJson.message[i]} alt="Picture of a ${breed} breed dog">`
+                `<img src=${responseJson.message[i]} alt="Picture of a ${breed} dog">`
             )
         }
     }
+    
+    $('#breed-type').empty().prepend(`${breed}`.toUpperCase());
+    $('#form-container').children('input:not(#submit)').val('');
 }
-
 
 const getBreed = () => {
     $('main').on('submit', event => {
         event.preventDefault();
         
         let breed = $('#breed').val();
-        const letters = /^[A-Za-z]+$/;
-        const clearField = $('.dog-image').empty() && $('.error-container').empty();
+        const letters = /^[a-z][a-z\s]*$/;
+        const clearField = $('.dog-image').empty() && $('.error-container').empty() && $('#breed-type').empty();
 
         if (breed.match(letters)) {
-            clearField;
             getDogImages(breed);
+            clearField;
         }
         else {
+            $('.error-container').prepend('Please input alphabetical characters only.');
+            $('#form-container').children('input:not(#submit)').val('');
             clearField;
-            $('.error-container').prepend('Please input alphabet characters only.');
         }
     })
 }
-
 
 const main = () => {
     getBreed();
